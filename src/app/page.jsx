fdec4d72864968946c62d3e1988ce3e529a3d07d
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-05 15:36:17 +0300

    added global.css and tried tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
     <div>
-      Page
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
