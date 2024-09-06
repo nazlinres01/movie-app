3fa17a3dee5297a5df9b740661f56ef390f3527d
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-06 09:14:04 +0300

    edited Tabs style , created key (https://www.themoviedb.org/)

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-import Link from "next/link"; // Link'i doğru şekilde import ediyoruz
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const Tabs = () => {
+  const searchParams = useSearchParams();
+  const genre = searchParams ? searchParams.get("genre") : null;
+  const [activeGenre, setActiveGenre] = useState(genre);
+
+  useEffect(() => {
+    setActiveGenre(genre);
+  }, [genre]);
+
   const tabs = [
     {
       name: "En Popüler",
@@ -19,10 +29,12 @@ const Tabs = () => {
 
   return (
     <div className="p-5 m-5 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7">
-      {tabs.map((tab, i) => (
+      {tabs.map((tab) => (
         <Link
-          key={i} // Her eleman için key eklemeyi unutmayın
-          className="cursor-pointer hover:opacity-75 transition-opacity"
+          key={tab.url}
+          className={`cursor-pointer hover:opacity-75 transition-opacity ${
+            activeGenre === tab.url ? "underline underline-offset-8 text-amber-600" : ""
+          }`}
           href={`/?genre=${tab.url}`}
         >
           {tab.name}
